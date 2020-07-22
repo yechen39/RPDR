05156a7423e4f361c121a25400e42a461c34a80d
yechen39 <48919927+yechen39@users.noreply.github.com>
2020-07-22 19:13:23 -0400

    Update RPDR_parsing.py
    adjust the matching
    add new class
    add new functions

--- RPDR_parsing.py
+++ RPDR_parsing.py
@@ -2,6 +2,8 @@ import argparse
 import pandas as pd
 import re
 import sqlite3
+import json
+
 
 class RPDR_query():
     instance_count = 0
@@ -10,9 +12,14 @@ class RPDR_query():
         self.name = name
         self.filein = filein
         self.instance_count += 1
+        print(self.get_attr())
 
     def get_attr(self):
-        print(' NAME :- {} \n SOURCE FILE :- {} \n INSTANCE COUNT :- {}'.format(self.name, self.filein, self.instance_count))
+        filelist = self.parse_filelist()
+        with open(filelist[0], 'r') as f:
+            header = f.readline()
+            header = header.strip().split('|')
+        print('NAME :- {} \n SOURCE FILE :- {} \n COLUMNS INCLUDED :- {} \n INSTANCE COUNT :- {}'.format(self.name, self.filein, header, self.instance_count))
 
     def parse_filelist(self):
         filelist = []
@@ -52,11 +59,34 @@ class RPDR_query():
             print("Multiple MRNs per obeservation!")
         else:
             print("Single MRN per obeservation!")
+        return header, data
 
-        df = pd.DataFrame(data, columns=header)
-        return df
+class matchterm():
+    instance_count = 0
 
-def parsing_keywords(fileitem):
+    def __init__(self, name, filein):
+        self.name = name
+        self.filein = filein
+        self.instance_count += 1
+        print(self.get_attr())
+
+    def get_attr(self):
+        with open(self.filein, 'r') as f:
+            data = json.load(f)
+        jstr = json.dumps(data, indent=4)
+        print('NAME :- {} \n SOURCE FILE :- {} \n MATCHTERM :- {} \n INSTANCE COUNT :- {}'.format(self.name, self.filein, jstr, self.instance_count))
+
+    def parse_matchterm(self):
+        with open(self.filein, 'r') as f:
+            data = json.load(f)
+        for key, value in data.items():
+            if value["Method"] == "exact":
+                value["KP"] = parse_keywords(value["Path"])
+            if value["Method"] == "pattern":
+                value["KP"] = parse_patterns(value["Path"])
+        return data
+
+def parse_keywords(fileitem):
     keywords = []
     with open(fileitem, 'r') as f:
         for line in f:
@@ -69,8 +99,15 @@ def parse_patterns(fileitem):
     with open(fileitem, 'r') as f:
         patterns = '\\b|\\b'.join(line.strip() for line in f)
         patterns = '\\b' + patterns + '\\b'
+        rpatterns = re.compile(patterns, re.IGNORECASE)
     f.close()
-    return patterns
+    return rpatterns
+
+def kp_match(item, match, method):
+    if method == "exact":
+        return item in match
+    if method == "pattern":
+        return bool(match.search(item))
 
 def create_connection(db_file):
     conn = None
@@ -88,9 +125,31 @@ def sqlout(name=None, df=None, db_file=''):
     conn = sqlite3.connect(db_file)
     df.to_sql(name, conn, if_exist='replace', index=False)
 
-def read_matched(data=None, matchtype="exact", byvar=None, flagname = None, reffl=None, outcols=[],
+def testitemout(rpdrobj=None, testitem=None):
+    filelist = rpdrobj.parse_filelist()
+    with open(filelist[0], 'r') as f:
+        header = f.readline()
+        header = header.strip().split('|')
+    f.close()
+    testloc = [i for i, j in enumerate(header) if j in testitem]
+    item = set()
+    for file in filelist:
+        with open(file, 'r') as f:
+            for line in f:
+                if 'EMPI|' not in line:
+                    parse_line = line.strip().split('|')
+                    item.add(parse_line[testloc])
+        f.close()
+    return item
+
+
+
+
+
+def read_matched(rpdrobj=None, matchtermobj=None, outtype="pd",
                  timevar=None, sdate=None, edate=None,
-                 outtype="pd", sqltblname=None, connection=None, subset=True):
+                 outcols=[],
+                 sqltblname=None, connection=None):
     # data : pandas dataframe
     # matchtype : string(possible value ["exact", "pattern"])
     # byvar : string(name for the variable to be matched upon)
@@ -104,43 +163,53 @@ def read_matched(data=None, matchtype="exact", byvar=None, flagname = None, reff
     # sqltblname: string(name for output sql table, necessary of outtype is "sql")
     # subset: boolean(whether the final output will be a subset or a full set based on rows)
 
-    # errors for necessary input
-    datacopy = data.copy()
-    # optional time matching
-    if timevar:
-        datacopy[str(timevar)] = pd.to_datetime(datacopy[str(timevar)])
-        datacopy = datacopy.sort_values(['EMPI', str(timevar)])
-        if not edate and sdate:
-            datacopy = datacopy.loc[(datacopy[str(timevar)] >= sdate)]
-        if edate and not sdate:
-            datacopy = datacopy.loc[(datacopy[str(timevar)] <= edate)]
-        if edate and sdate:
-            datacopy = datacopy.loc[(datacopy[str(timevar)] >= sdate) & (datacopy[str(timevar)] <= edate)]
-    # matching
-    byflag = str(flagname) + "_flag"
-    print(byflag)
-    # match based on exact keywords
-    if matchtype == "exact":
-        ref = parsing_keywords(reffl)
-        datacopy[str(byflag)] = datacopy[str(byvar)].apply(lambda x: x in ref)
-    if matchtype == "pattern":
-        ref = parse_patterns(reffl)
-        rref = re.compile(ref, re.IGNORECASE)
-        datacopy[str(byflag)] = datacopy[str(byvar)].apply(lambda x: bool(rref.search(x)))
-    # check subset/fullset
-    if subset:
-        datacopy = datacopy.loc[datacopy[str(byflag)]]
-    # columns specified for output
-    if outcols:
-        outcols.append(str(byflag))
-        datacopy = datacopy[[outcols]]
+    filelist = rpdrobj.parse_filelist()
+    matchterm = matchtermobj.parse_matchterm()
+    n = 0
+    data = []
+    header = []
+    mloc = []
+    MRN_len = 0
+    IDlist = set()
+    for file in filelist:
+        with open(file, 'r') as f:
+            for line in f:
+                if n == 0:
+                    parse_line = line.strip().split('|')
+                    header = parse_line
+                    mloc = [i for i, j in enumerate(header) if j in matchterm.keys]
+                n += 1
+                if 'EMPI|' not in line:
+                    # map
+                    parse_line = line.strip().split('|')
+                    match = list(map(kp_match, parse_line[mloc], matchterm.values["KP"], matchterm.values["Method"]))
+                    if sum(match):
+                        data.append(parse_line)
+                        IDlist.add(parse_line[0])
+                        MRN_len = max(MRN_len, len(parse_line[3].split(',')))
+        f.close()
+    print('File {} includes {} subjects with {} rows'.format(str(rpdrobj), len(IDlist), n - len(filelist)))
 
-    datacopy.reset_index()
+
     # output format
+    df = pd.DataFrame(data=data,columns=header)
+    if timevar:
+        df[str(timevar)] = pd.to_datetime(df[str(timevar)])
+        if not edate and sdate:
+            df = df.loc[df[str(timevar)] >= sdate]
+        if edate and not sdate:
+            df = df.loc[df[str(timevar)] >= edate]
+        if edate and sdate:
+            df = df.loc[(df[str(timevar)] >= sdate) & (df[str(timevar)] <= edate)]
+
+    if outcols:
+        df = df[[outcols]]
+
     if outtype == "pd":
-        return datacopy
+        return df
     if outtype == "sql":
-        sqlout(name=sqltblname, df=datacopy, db_file=connection)
+        sqlout(name=sqltblname, df=df, db_file=connection)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Scripts for RPDR file pre-processing')
@@ -150,4 +219,3 @@ if __name__ == '__main__':
     filein = args.F
     name = args.N
     datfl = RPDR_query(name=name, filein=filein)
-    print(datfl.get_attr())
